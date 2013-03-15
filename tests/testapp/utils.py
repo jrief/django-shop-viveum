b2cba1e2da3b1bd232ece925197db8ec050150b4
Jacob Rief <jacob.rief@gmail.com>
2013-03-15 17:32:38 +0100

    Separated what_is_my_ip into utility.py

---
+++ tests/testapp/utils.py
@@ -0,0 +1,21 @@
+import httplib2
+from pyquery.pyquery import PyQuery
+
+
+def what_is_my_ip():
+    """
+    Return the remote IP address as seen by the outside world.
+    """
+    conn = httplib2.Http()
+    httpresp, content = conn.request('http://findwhatismyipaddress.org/', method='GET')
+    assert(httpresp.status == 200)
+    dom = PyQuery(content)
+    elements = dom('#ipadd_n')
+    assert(elements)
+    td = elements('td')
+    assert(td and len(td) >= 2)
+    return td[1].text
+
+
+if __name__ == "__main__":
+    print 'My IP address is: %s' % what_is_my_ip()
